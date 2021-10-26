782acea68a5eaeaef2579ca5c696fa522a5e1de6
Brian Szekely <bszekely@nevada.unr.edu>
2021-10-26 14:05:52 -0700

    add commands

--- git_commands.py
+++ git_commands.py
@@ -8,7 +8,14 @@ Created on Tue Oct 19 14:36:21 2021
 Automate Github bash commands only works for Linux 
 """
 import subprocess
+import yaml
 
+def import_yaml():
+    message1 = input("\nYAML Directory\n")
+    with open(message1, "r") as stream:
+        documents = yaml.safe_load(stream)
+        return documents['Token'],documents['repo'],documents['branch'],documents['branch_exist']
+      
 def run(*args):
     return subprocess.check_call(['git'] + list(args))
 
@@ -20,18 +27,33 @@ def initialize():
         print("git is already initialized, proceed")
 
 def add():
-    message = input("\nDo you want to git add all files or specific? all/specific")
+    message = input("\nDo you want to git add all files or specific? all/specific/n")
     if message == "all":
         run("add","--all")
     elif message == "specific":
-        message2 = input("\nwhat files would you like to input ")
-        run(message2)
-    
-    
+        message2 = input("\nwhat files would you like to input\n")
+        run("add", f'{message2}')
+def commit():
+    message = input("\ncommit message:\n")
+    run('commit', "-m", message)
+
+def branch():
+    message = input("\nDo you want to push these changes? (y/n)\n")
+   
 if __name__ == '__main__':
-    command_prompt = input("input what git command you want")
-    command_prompt = command_prompt.lower()
-    if command_prompt == "add":
-        add()
-    elif command_prompt == "init":
-        initialize()
+    import_yaml()
+    while True:
+        command_prompt = input("\ninput what git command you want\n")
+        command_prompt = command_prompt.lower()
+        if command_prompt == "add":
+            add()
+        elif command_prompt == "init":
+            initialize()
+        elif command_prompt == "commit":
+            commit()
+        elif command_prompt == "branch":
+            branch()
+        # elif command_prompt == "push":
+        #     #push()
+        elif command_prompt == "end":
+            break
